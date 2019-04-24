d816c4fa7a6e2163ac46a6adb8bc2795b58c6519
Student <example@example.com>
2019-04-24 15:47:07 -0700

    add photos to Jirn

--- src/Jirn/Jirn.js
+++ src/Jirn/Jirn.js
@@ -1,11 +1,31 @@
 import React, {Component } from 'react';
 import styled from 'styled-components';
-
+import  pic  from '../assets/img/Jirn1.png';
+import picTwo from '../assets/img/Jirn2.png';
+import picThree from '../assets/img/Jirn3.png';
+import picFour from '../assets/img/Jirn4.png';
 
 function Jirn() {
 
+  const Img = styled.img`
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 500px;
+  height:500px;
+  `;
+
+  const ImgTwo = styled.img`
+  width: 700px;
+  height:300px;
+  `;
+
   return(
 <div>
+<Img src = { pic }/>
+<Img src = { picTwo }/>
+<ImgTwo src = { picThree }/>
+<ImgTwo src = { picFour }/>
 <p>Test</p>
 </div>
 
